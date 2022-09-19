7edc9c2ee40bd235f3b333943827c92e2a1130d3
Timerson Downing <tldown2222@gmail.com>
2022-09-19 18:10:59 -0500

    Creates Maze With Coins No Pacman

---
+++ script.js
@@ -0,0 +1,35 @@
+var world = [
+    [2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,1,2,1,2,2,2,1,1,1,1,1,2],
+    [2,1,2,1,2,2,2,1,2,2,2,1,2],
+    [2,1,1,1,0,0,1,1,1,1,1,1,2],
+    [2,1,2,1,2,2,2,1,1,1,1,1,2],
+    [2,1,2,1,1,0,1,1,2,2,1,1,2],
+    [2,0,2,1,2,2,1,1,2,2,1,1,2],
+    [2,0,1,1,0,0,1,2,2,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2]
+]
+
+function displayWorld() {
+    var output = '';
+    for (var i=0; i<world.length; i++){
+        output += "\n<div class='row'>"
+        for(var j=0; j<world[i].length; j++){
+            if(world[i][j] == 2){
+                output += "\n\t<div class='brick'></div>";
+            }
+            else if(world[i][j] == 1){
+                output += "\n\t<div class='coin'></div>";
+            }
+            else if(world[i][j] == 0){
+                output += "\n\t<div class='empty'></div>";
+            }
+        }
+        output += "\n</div>"
+    }
+
+    console.log(output);
+    document.getElementById('content').innerHTML = output;
+}
+
+displayWorld();
